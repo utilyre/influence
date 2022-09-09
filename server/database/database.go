f84559862d226c20342e03437c9b0bc17a90c5fb
Amirabbas Fazelinia <utilyre@proton.me>
2022-09-09 21:16:37 +0430

    stablize connection between server and database

---
+++ server/database/database.go
@@ -0,0 +1,44 @@
+package database
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var Instance *gorm.DB
+
+func Connect() error {
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("DATABASE_USER"),
+		os.Getenv("DATABASE_PASS"),
+		os.Getenv("DATABASE_HOST"),
+		os.Getenv("DATABASE_PORT"),
+		os.Getenv("DATABASE_NAME"),
+	)
+
+	var (
+		db  *gorm.DB
+		err error
+	)
+	for i := 0; i < 10; i++ {
+		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err != nil {
+			continue
+		}
+
+		break
+	}
+
+	if err != nil {
+		return err
+	}
+
+	// TODO: automigrate
+
+	Instance = db
+	return nil
+}
--- server/main.go
+++ server/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"server/database"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	err := database.Connect()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello world")
+	})
+
+	addr := fmt.Sprintf("0.0.0.0:%s", os.Getenv("SERVER_PORT"))
+	app.Listen(addr)
 }
